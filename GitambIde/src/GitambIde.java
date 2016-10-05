f2505eb829b21361eada8108bce984ca57f8742d
Kungfu faiter <Kungfu faiter@Kungfufaiter>
2016-10-05 23:15:27 +0200

    4aversio

--- GitambIde/src/GitambIde.java
+++ GitambIde/src/GitambIde.java
@@ -2,7 +2,7 @@
 public class GitambIde {
 
 	public static void main(String[] args) {
-		int variable=200;
+		int variable=400;
 		String a="brusli and the mother";
 		System.out.println("amos a ver que passa con Git" + a);
 
